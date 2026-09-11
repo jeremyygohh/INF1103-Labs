343f9ff4fd07544c97755d756c5742a1b98687d1
Jeremy <2605327@sit.singaporetech.edu.sg>
2026-09-11 22:30:32 +0800

    Added overstock alert and final reporting

--- auditor.py
+++ auditor.py
@@ -1,4 +1,5 @@
 inventory = 0
+failed_entries = 0
 
 while True:
     stock = input("Enter stock quantity: ")
@@ -8,11 +9,20 @@ while True:
 
     elif stock.startswith("-"):
         print("Negative numbers are not allowed")
+        failed_entries += 1
         continue
 
     elif not stock.isdigit():
         print("Invalid input")
+        failed_entries += 1
         continue
 
     stock = int(stock)
     inventory += stock
+
+    if inventory > 500:
+        print("Inventory Exceeded")
+        break
+
+print("Total Units Processed:", inventory)
+print("Number of Failed/Rejected Entries:", failed_entries)
