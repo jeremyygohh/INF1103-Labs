88653749e67a219d9be8623bc3b0bbce9bd80c24
Jeremy <2605327@sit.singaporetech.edu.sg>
2026-09-17 23:00:25 +0800

    Added final reporting function

--- modular_auditor.py
+++ modular_auditor.py
@@ -21,7 +21,12 @@ def get_valid_input():
 
 def process_delivery(current_total, new_value):
     new_total = current_total + new_value
+    return new_total
 
 def calculate_tax(amount):
     tax = amount * 0.10
     return tax
+
+def generate_report(total_units, failed_attempts):
+    print("Total Units Processed:", total_units)
+    print("Number of Failed/Rejected Entries:", failed_attempts)
