56d4c677c08aba69cf9f9c3122659c042343b251
Jeremy <2605327@sit.singaporetech.edu.sg>
2026-09-18 22:20:02 +0800

    Edited process delivery function

--- modular_auditor.py
+++ modular_auditor.py
@@ -24,8 +24,7 @@ def get_valid_input():
         return stock, failed
     
 def process_delivery(current_total, new_value):
-    new_total = current_total + new_value
-    return new_total
+    return current_total + new_value
 
 def calculate_tax(amount):
     tax = amount * 0.10
